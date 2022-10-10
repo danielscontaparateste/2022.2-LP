2bd157e78ab3c8cf878fc688d965fe3f13e90e36
Daniel Ferreira <danielscontaparateste@gmail.com>
2022-10-10 09:44:47 -0300

    Aula 10/10

--- ponteiros/maiorMenor.c
+++ ponteiros/maiorMenor.c
@@ -6,28 +6,27 @@
 #define MX 100 // Quant. de elementos na faixa de valores do vetor
 #define INI 0
 
-void getMenor(int *pd, int qtd, int *pMenor);
-void getMaior(int *pd, int qtd, int *pMaior);
-void imprimirVetor(int *pd, int qtd);
+void getMenor(int *, int, int *);
+void getMaior(int *, int, int *);
+void imprimirVetor(int *, int);
+void preencherVetor(int *, int, int, int);
+int *getEndMenor(int *, int);
+int *getEndMaior(int *, int);
 
 int main(){
     int v[TAM], maior, menor;
-    int *pv = NULL; // int *pv = v;
+    
+    
 
-    srand(time(NULL));
-
-    pv = v; //pv = &v[0];
-    for (int k=0; k<TAM; k++){
-        // *(pv+k) = INI + rand() % MX; 
-        *(v+k) = INI + rand() % MX;
-    }
+    preencherVetor(v, TAM, INI, MX);
+    
     imprimirVetor(v, TAM);
 
     printf("---\n");
     getMenor(v, TAM, &menor);
     getMaior(v, TAM, &maior);
 
-    printf("Menor: %d\nMaior: %d\n",maior, menor);
+    printf("[%p] Menor: %d\n[%p] Maior: %d\n",getEndMenor(v,TAM), menor, getEndMaior(v,TAM), maior);
 
     return 0;
 
@@ -49,7 +48,31 @@ void getMaior(int *pd, int qtd, int *pMaior){
 
 void imprimirVetor(int *pd, int qtd){
     for (int k=0; k<qtd; k++){
-        printf("%d ",*(pd+k));
+        printf("[%p] %d\n",pd+k,*(pd+k));
     }
-    printf("\n");
+}
+
+void preencherVetor(int *pd, int qtd, int inicial, int qtdFaixa){
+    srand(time(NULL));
+    for (int k=0; k<qtd; k++){
+        *(pd+k) = inicial + rand() % qtdFaixa;
+    }
+}
+
+int *getEndMenor(int *pd, int qtd){
+    int *pm = NULL;
+    pm = pd;
+    for (int k=1; k<qtd; k++){
+        pm = ( *(pd+k) < *pm )?(pd+k):pm;
+    }
+    return pm;
+}
+
+int *getEndMaior(int *pd, int qtd){
+    int *pm = NULL;
+    pm = pd;
+    for (int k=1; k<qtd; k++){
+        pm = ( *(pd+k) > *pm )?(pd+k):pm;
+    }
+    return pm;
 }
