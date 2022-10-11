14cd9a807100abf1c6758034b6d53d2d3eb331c6
Daniel Ferreira <danielscontaparateste@gmail.com>
2022-10-11 12:00:04 -0300

    11/10

--- ponteiros/matriz/mat.c
+++ ponteiros/matriz/mat.c
@@ -4,7 +4,9 @@
 
 void preencherVetor(int *, int, int, int);
 void imprimirMatriz(int *, int, int);
+void imprimirMatriz2(int *, int, int);
 void imprimirVetor(int *, int );
+int getValorMatriz(int *,int, int, int);
 
 int main(){
 
@@ -27,8 +29,23 @@ int main(){
     preencherVetor(pData, nl*nc, 0, 100);
     puts("--------");
     imprimirVetor(pData, nl * nc);
-    puts("--------");
+    puts("-------- Função imprimirMatriz --------");
     imprimirMatriz(pData, nl, nc);
+    puts("-------- Função imprimirMatriz2 --------");
+    imprimirMatriz2(pData, nl, nc);
+
+    puts("--------");
+
+    int a, b;
+    puts("Digite a coordenada do valor que deseja recuperar: ");
+    puts("linha: ");
+    scanf("%d",&a);
+    puts("coluna: ");
+    scanf("%d",&b);
+
+    /* Exercício: implemente uma verificação dos limites de a e b recebidos */
+
+    printf("Valor recuperado %d\n",getValorMatriz(pData,a,b,nc));
 
     free(pData);
 
@@ -46,18 +63,28 @@ void preencherVetor(int *pd, int qtd, int inicial, int qtdFaixa){
 }
 
 void imprimirMatriz(int *pd, int qnl, int qnc){
-
     for (int k=0; k<qnl; k++){
         for (int j=0; j< qnc; j++){
             printf("%3d ",*(pd + qnc * k + j));
         }
         printf("\n");
     }
+}
 
+void imprimirMatriz2(int *pd, int qnl, int qnc){
+    for (int k=0; k<qnl*qnc; k++){
+        if (!(k%qnc)) printf("\n");
+        printf("%3d ",*(pd+k));
+    }
+    printf("\n");
 }
 
 void imprimirVetor(int *pd, int qtd){
     for (int k=0; k<qtd; k++){
         printf("[%p] %d\n",pd+k,*(pd+k));
     }
+}
+
+int getValorMatriz(int *pd,int lin, int col, int qnc){
+    return *(pd + (lin-1)*qnc + (col-1));
 }
