cddc59054d9571e988287cd267301e0924d8eb03
Daniel Ferreira <danielscontaparateste@gmail.com>
2022-11-01 11:24:00 -0300

    01/30

---
+++ Estruturas/s1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define TC 3
+#define TD 50
+
+struct UF{
+    char codigo[TC];
+    char descricao[TD];
+};
+
+int main(){
+    struct UF f1;
+
+    puts("Digite o codigo da UF: ");
+    fgets(f1.codigo,TC,stdin);
+
+    fflush(stdin);
+
+    puts("Digite a descricao da UF: ");
+    fgets(f1.descricao,TD,stdin);
+
+    puts("Dados digitados");
+
+    printf("Codigo: %s\n",f1.codigo);
+    printf("Descricao: %s\n",f1.descricao);
+
+    return 0;
+
+}
